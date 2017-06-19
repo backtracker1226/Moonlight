ea4cabb656675943d551a640476c0667d2d9cee2
Gru <gru325@gmail.com>
2017-06-19 19:34:57 +0900

    First Re

--- src/main/java/org/moon/controller/HomeController.java
+++ src/main/java/org/moon/controller/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-/**
+/***
  * Handles requests for the application home page.
  */
 @Controller
